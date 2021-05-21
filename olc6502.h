c3afed823debfe53404d3d3dfc4f9708c2c4487d
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-21 14:30:46 -0400

    functions

--- olc6502.h
+++ olc6502.h
@@ -31,9 +31,19 @@ public:
 
     void ConnectBus(Bus *n) { bus = n; }
 
+	uint8_t IMP();	uint8_t IMM();	
+	uint8_t ZP0();	uint8_t ZPX();	
+	uint8_t ZPY();	uint8_t REL();
+	uint8_t ABS();	uint8_t ABX();	
+	uint8_t ABY();	uint8_t IND();	
+	uint8_t IZX();	uint8_t IZY();
+
 private:
     Bus     *bus = nullptr;
     uint8_t read(uint16_t a);
     void    write(uint16_t a, uint8_t d);
+
+    uint8_t GetFlag(FLAG6502 f);
+    void    SetFlag(FLAG6502 f, bool v);
 
 };
