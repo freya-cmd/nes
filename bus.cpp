724aab202e3bcc69245c14183b1916634a3b61bc
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-21 13:44:12 -0400

    add

--- bus.cpp
+++ bus.cpp
@@ -14,10 +14,13 @@ Bus::~Bus()
 
 void Bus::write(uint16_t addr, uint8_t data)
 {
-ram[addr] = data;
+    if (addr >= 0x0000 && <= 0xFFFF)
+        ram[addr] = data;
 }
 
 uint8_t Bus::read(uint16_t addr, boolReadOnly)
 {
-
+    if (addr >= 0x0000 && <= 0xFFFF)
+        return ram[addr];
+        return 0x00;
 }
