a82cbdf8a4f9167096f0c69f33189ac66ea4d9ef
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-23 15:40:32 -0400

    reorg

--- olc6502.h
+++ olc6502.h
@@ -1,42 +1,90 @@
 #pragma once
 #include <vector>
+
 #include <string>
 #include <map>
+
 #ifdef LOGMODE
 #include <stdio.h>
 #endif
 
+
 class Bus;
+
 
 class olc6502
 {
-    public: 
-        olc6502();
-        ~olc6502();
+public:
+	olc6502();
+	~olc6502();
 
 public:
-    enum FLAGS6502
-    {
-        C = (1 << 0),
-        Z = (1 << 1),
-        I = (1 << 2),
-        D = (1 << 3),
-        B = (1 << 4),
-        U = (1 << 5),
-        V = (1 << 6),
-        N = (1 << 7),
-    };
-
-    uint8_t  a      = 0x00;
-	uint8_t  x      = 0x00;
+	
+	uint8_t  a      = 0x00;
+	uint8_t  x      = 0x00;	
 	uint8_t  y      = 0x00;
 	uint8_t  stkp   = 0x00;
-	uint16_t pc     = 0x0000;
-	uint8_t  status = 0x00;		
+	uint16_t pc     = 0x0000;	
+	uint8_t  status = 0x00;	
+	
 
-    bool complete();
-    void ConnectBus(Bus *n) { bus = n; }
-    std::map<uint16_t, std::string> disassemble(uint16_t nStart, uint16_t nStop);
+	void reset();	
+	void irq();		
+	void nmi();		
+	void clock();	
+
+
+
+	void ConnectBus(Bus *n) { bus = n; }
+
+
+	std::map<uint16_t, std::string> disassemble(uint16_t nStart, uint16_t nStop);
+
+	enum FLAGS6502
+	{
+		C = (1 << 0),	
+		Z = (1 << 1),	
+		I = (1 << 2),	
+		D = (1 << 3),	
+		B = (1 << 4),	
+		U = (1 << 5),	
+		V = (1 << 6),	
+		N = (1 << 7),	
+	};
+
+private:
+	uint8_t GetFlag(FLAGS6502 f);
+	void    SetFlag(FLAGS6502 f, bool v);
+	
+
+	uint8_t  fetched     = 0x00;   
+	uint16_t temp        = 0x0000; 
+	uint16_t addr_abs    = 0x0000; 
+	uint16_t addr_rel    = 0x00;   
+	uint8_t  opcode      = 0x00;   
+	uint8_t  cycles      = 0;	   
+	uint32_t clock_count = 0;	   
+
+	Bus     *bus = nullptr;
+	uint8_t read(uint16_t a);
+	void    write(uint16_t a, uint8_t d);
+
+	uint8_t fetch();
+
+
+
+	struct INSTRUCTION
+	{
+		std::string name;		
+		uint8_t     (olc6502::*operate )(void) = nullptr;
+		uint8_t     (olc6502::*addrmode)(void) = nullptr;
+		uint8_t     cycles = 0;
+	};
+
+	std::vector<INSTRUCTION> lookup;
+	
+private: 
+
 
 	uint8_t IMP();	uint8_t IMM();	
 	uint8_t ZP0();	uint8_t ZPX();	
@@ -45,8 +93,10 @@ public:
 	uint8_t ABY();	uint8_t IND();	
 	uint8_t IZX();	uint8_t IZY();
 
-    //OPCODES
-    uint8_t ADC();	uint8_t AND();	uint8_t ASL();	uint8_t BCC();
+private: 
+
+
+	uint8_t ADC();	uint8_t AND();	uint8_t ASL();	uint8_t BCC();
 	uint8_t BCS();	uint8_t BEQ();	uint8_t BIT();	uint8_t BMI();
 	uint8_t BNE();	uint8_t BPL();	uint8_t BRK();	uint8_t BVC();
 	uint8_t BVS();	uint8_t CLC();	uint8_t CLD();	uint8_t CLI();
@@ -61,35 +111,11 @@ public:
 	uint8_t STX();	uint8_t STY();	uint8_t TAX();	uint8_t TAY();
 	uint8_t TSX();	uint8_t TXA();	uint8_t TXS();	uint8_t TYA();
 
-    uint8_t XXX();
 
-    void clock();
-    void reset();
-    void irq();
-    void nmi();
+	uint8_t XXX();
 
-    uint8_t fetch();
-    uint8_t fetched = 0x00;
-    uint16_t addr_abs = 0x0000;
-    uint16_t addr_rel = 0x00;
-    uint8_t opcode = 0x00;
-    uint8_t cycles = 0;
-
+#ifdef LOGMODE
 private:
-    Bus     *bus = nullptr;
-    uint8_t read(uint16_t a);
-    void    write(uint16_t a, uint8_t d);
-
-    uint8_t GetFlag(FLAG6502 f);
-    void    SetFlag(FLAG6502 f, bool v);
-
-    struct INSTRUCTION
-	{
-		std::string name;		
-		uint8_t  (olc6502::*operate )(void) = nullptr;
-		uint8_t  (olc6502::*addrmode)(void) = nullptr;
-		uint8_t  cycles = 0;
-	};
-
-    std::vector<INSTRUCTION> lookup;
+	FILE* logfile = nullptr;
+#endif
 };
