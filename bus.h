17c21ddb277dbc606aea3fd115657fb17b829549
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-24 10:01:23 -0400

    include and cart add with cpu

--- bus.h
+++ bus.h
@@ -1,20 +1,36 @@
 #pragma once
 #include <cstdint>
-#include "olc6502.h"
 #include <array>
+#include "olc6502.h"
+#include "olc2C02.h"
+#include "Cartridge.h"
 
 class Bus
 {
-    public:
-        Bus();
-         ~Bus();
-
 public:
-    olc6502 cpu;
-
-    std::array<uint8_t, 64 * 1024> ram;
+	Bus();
+	~Bus();
 
 public: 
-    void write(uint16_t addr, uint8_t data);
-    uint8_t read(uint16_t addr, bool bReadOnly = false);
+
+	olc6502 cpu;	
+	olc2C02 ppu;
+	std::shared_ptr<Cartridge> cart;
+	uint8_t cpuRam[2048];
+
+public: 
+	void    cpuWrite(uint16_t addr, uint8_t data);
+	uint8_t cpuRead(uint16_t addr, bool bReadOnly = false);
+
+private:
+
+	uint32_t nSystemClockCounter = 0;
+
+public: 
+
+	void insertCartridge(const std::shared_ptr<Cartridge>& cartridge);
+
+	void reset();
+
+	void clock();
 };
